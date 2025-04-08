a438161cf97b312dcedb4b6e30c0fb18ec6c53e0
Gustbarbr <gustbarbr@gmail.com>
2025-04-08 18:38:40 -0300

    Adicionando chapeu de redução de custo de FL

---
+++ A Place Between/Assets/Game Folder/Scripts/HatScripts/HatOfFLCostReduction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatOfFLCostReduction : MonoBehaviour
+{
+    PlayerControl player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControl>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            player.hatOfMiner = true;
+        }
+    }
+}
